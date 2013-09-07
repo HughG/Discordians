b5854a6d8ddadbcf38a2f7afb71b0b59fc8270ce
Hugh Greene <hughg_tameter_org>
2013-09-07 11:08:11 +0100

    Update setup & build instructions for protocol23 being factored out. Clarify licensing of OFFO Hyphenation.

---
+++ SConsLocal/Config_MacOS_macports.py
@@ -0,0 +1,7 @@
+def Config(env):
+    env['RUBY'] = '/opt/local/bin/ruby1.9'
+    env['DOT'] = '/Applications/Graphviz.app/Contents/MacOS/dot'
+    env['XSLT'] = '/opt/local/bin/xsltproc'
+    env['ASCIIDOC'] = '/opt/local/bin/asciidoc'
+    env['A2X'] = '/opt/local/bin/a2x'
+    env['SHOW_PDF'] = '/usr/bin/osascript src/protocol23/tools/show_pdf.scpt'

3b4f966ed89f7d476ea1a3b3aa4b01cdb2f727c0
Hugh Greene <hughg_tameter_org>
2014-01-19 18:05:52 +0000

    Add git (for platform-independence and version info) and java (just for version info)

--- SConsLocal/Config_MacOS_macports.py
+++ SConsLocal/Config_MacOS_macports.py
@@ -1,4 +1,6 @@
 def Config(env):
+    env['GIT'] = '/opt/local/bin/git'
+    env['JAVA'] = '/usr/bin/java'
     env['RUBY'] = '/opt/local/bin/ruby1.9'
     env['DOT'] = '/Applications/Graphviz.app/Contents/MacOS/dot'
     env['XSLT'] = '/opt/local/bin/xsltproc'
